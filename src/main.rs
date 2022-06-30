672feca703f9549f6d7d81eb602b957cc1a04bfb
ostap_sulyk <ostapsulyk@gmail.com>
2022-06-30 12:29:06 -0400

    backup

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,39 @@
-use std::fs;
+use rust_web_server::ThreadPool;
 use std::io::prelude::*;
 use std::net::{TcpListener, TcpStream};
+use std::thread;
+use std::time::Duration;
+use std::{fs, io};
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:5500").unwrap();
+    let pool = ThreadPool::new(4);
 
     for stream in listener.incoming() {
         let stream = stream.unwrap();
-        handle_connection(stream);
-        break;
+
+        pool.execute(|| {
+            handle_connection(stream);
+        });
     }
 }
 
 fn handle_connection(mut stream: TcpStream) {
     let mut buffer = [0; 1024];
     stream.read(&mut buffer).unwrap();
+
     let get = b"GET / HTTP/1.1\r\n";
+    let sleep = b"GET /sleep HTTP/1.1\r\n";
 
     let (status_line, file_name) = if buffer.starts_with(get) {
+        ("HTTP/1.1 200 OK", "hello")
+    } else if buffer.starts_with(sleep) {
+        thread::sleep(Duration::from_secs(5));
         ("HTTP/1.1 200 OK", "hello")
     } else {
         ("HTTP/1.1 404 NOT FOUND", "404")
     };
+
     let contents = fs::read_to_string(format!("./html/{}.html", file_name)).unwrap();
 
     let response = format!(
@@ -30,6 +42,7 @@ fn handle_connection(mut stream: TcpStream) {
         contents.len(),
         contents
     );
+
     stream.write(response.as_bytes()).unwrap();
     stream.flush().unwrap();
 }
